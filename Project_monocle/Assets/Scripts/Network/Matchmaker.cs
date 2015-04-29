7115942f13e3b2ac369bb19105ba0fc55d8bb89d
Pascal <pasquical@msn.com>
2015-04-29 17:50:54 -0400

    Début network, connection entre deux joueurs dans une RandomRoom

---
+++ Project_monocle/Assets/Scripts/Network/Matchmaker.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Matchmaker : MonoBehaviour 
+{
+	private int playerCount;
+
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("v1.0");
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionState.ToString());
+	}
+
+	void OnJoinedLobby()
+	{
+		Debug.Log("Joined lobby");
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log ("Can't join random room");
+	}
+
+	void OnCreatedRoom()
+	{
+		Debug.Log ("room created");
+	}
+
+	void OnJoinedRoom()
+	{
+		Debug.Log ("room joined");
+
+		GameObject player = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+		Camera camera = player.transform.Find("Camera").GetComponent<Camera>();
+		camera.enabled = true;
+
+		Destroy(GameObject.Find("Canvas"));
+	}
+
+	public void CreateGame()
+	{
+		RoomOptions options = new RoomOptions();
+		options.maxPlayers = 2;
+		options.isOpen = true;
+		
+		PhotonNetwork.CreateRoom(null, options, null);
+	}
+
+	public void JoinGame()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+}
---
+++ Project_monocle/Assets/Scripts/Network/SimpleMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+
+	private float speed;
+	private PhotonView pv;
+
+	// Use this for initialization
+	void Start () 
+	{
+		speed = 0.1f;
+		pv = PhotonView.Get(this);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(pv.isMine)
+		{
+			if(Input.GetKey(KeyCode.W))
+			{
+				transform.Translate(transform.forward * speed);
+			}
+			if(Input.GetKey(KeyCode.S))
+			{
+				transform.Translate(-transform.forward * speed);
+			}
+			if(Input.GetKey(KeyCode.D))
+			{
+				transform.Translate(transform.right * speed);
+			}
+			if(Input.GetKey(KeyCode.A))
+			{
+				transform.Translate(-transform.right * speed);
+			}
+		}
+	}
+}
