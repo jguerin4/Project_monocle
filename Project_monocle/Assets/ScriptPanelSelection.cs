9198fcbff52bf89a0008e14eea3ccf71364eadf4
Éric Dionne <ericd_420@hotmail.com>
2015-05-28 20:14:46 -0400

    no message

---
+++ Project_monocle/Assets/ScriptPanelSelection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScriptPanelSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Image img =  GameObject.Find("MyPanel").GetComponent<Image>();
+		img.color = UnityEngine.Color.red;
+	
+	}
+}
---
+++ Project_monocle/Assets/Scripts/GUI/TestChangerPanel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestChangerPanel : MonoBehaviour {
+
+	public void ClickButton(string buttonID){
+		Debug.Log ("Click on " + buttonID);
+	}
+}
